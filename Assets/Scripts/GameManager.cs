572d9a0f3985cb737e84180d43e14bf122c5a200
Swidnixx <swidnixx@gmail.com>
2023-10-27 16:36:28 +0200

    Obstacle, Game Over and Restart

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,10 +13,25 @@ public class GameManager : MonoBehaviour
     #endregion
 
     public float worldSpeed = 2;
+    public Text scoreText;
+    public GameObject gameOverPanel;
     float score;
 
     private void Update()
     {
         score += worldSpeed * Time.deltaTime;
+        scoreText.text = score.ToString("F0");
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
     }
 }
