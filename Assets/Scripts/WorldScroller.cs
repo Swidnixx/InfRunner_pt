6a286fe055eb67115082b3b502935521ce1ce1fc
Swidnixx <swidnixx@gmail.com>
2023-11-03 16:35:56 +0100

    World scroller modification so it can handle different width segments. Changes segments bg gfx. Added platforms with platform effector. Started building laser obstacle

--- Assets/Scripts/WorldScroller.cs
+++ Assets/Scripts/WorldScroller.cs
@@ -17,12 +17,13 @@ public class WorldScroller : MonoBehaviour
 
         if(ground2.transform.position.x < 0)
         {
-            float d = ground1.sizeX;
-            ground1.transform.position = ground2.transform.position + new Vector3(d, 0, 0);
-
             Destroy(ground1.gameObject);
             WorldSegment newSegment = Instantiate(prefabSegments[Random.Range(0,prefabSegments.Length)]);
+
+            float d = ground2.sizeX * 0.5f + newSegment.sizeX * 0.5f;
+
             newSegment.transform.position = ground2.transform.position + new Vector3(d, 0, 0);
+
             ground1 = ground2;
             ground2 = newSegment;
         }
